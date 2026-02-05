1eb3a2341e892d98e1bc3120d6bd4ddbce961ae8
abubakarosafo11 <abubakarosafo11@gmail.com>
2026-02-05 20:43:03 +0000

    Delete ai-water-dashboard directory

--- ai-water-dashboard/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// NOTE: base:"./" makes it easier to host on GitHub Pages or any subpath.
-export default defineConfig({
-  plugins: [react()],
-  base: "./"
-});
