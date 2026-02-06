362a307580172bbb26795fa767321e8157fa67ca
abubakarosafo11 <abubakarosafo11@gmail.com>
2026-02-06 14:53:44 +0000

    Delete Bennyfilt_Pages_Ready directory

--- Bennyfilt_Pages_Ready/app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative base makes it work on GitHub Pages project sites (username.github.io/repo/)
-  base: "./"
-});
